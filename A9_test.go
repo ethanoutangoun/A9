3a78606e3826774909fa009f3617292c4ccadb63
yual-len <allenyu90@gmail.com>
2023-03-17 09:41:19 -0700

    first push for tests

--- A9_test.go
+++ A9_test.go
@@ -7,82 +7,76 @@ import (
 
 func TestInterpNum(t *testing.T) {
 
-	result := interp(numC{n: 2}, []Binding{})
-
-	if result.(float64) != 2 {
-		t.Errorf("interp num failed")
+	result := interp(NumC{N: 2}, []Binding{})
+	if (result != NumV{N: 2}) {
+		t.Errorf("Expected NumV(2)")
 	}
 
 }
 
 func TestInterpId(t *testing.T) {
 
-	result := interp(idC{s: "x"}, []Binding{{name: "y", val: 4},
-		{name: "x", val: 2}})
-	if result.(int) != 2 {
-		t.Errorf("idC interp failed")
+	result := interp(IdC{S: "x"}, []Binding{{Name: "y", Val: NumV{N: 4}},
+		{Name: "x", Val: NumV{N: 2}}})
+	if (result != NumV{N: 2}) {
+		t.Errorf("Expected NumV(2)")
 	}
 
 }
 
 func TestInterpString(t *testing.T) {
 
-	result := interp(strC{str: "hello"}, []Binding{})
-	if result != "hello" {
-		t.Errorf("interp string")
+	result := interp(StrC{S: "hello"}, []Binding{})
+	if (result != StrV{S: "hello"}) {
+		t.Errorf("Expected NumV(2)")
 	}
 
 }
 
 func TestLamC(t *testing.T) {
-	result := interp(lamC{args: []string{"x"}, body: appC{fun: idC{s: "+"}, args: []ExprC{idC{s: "x"}, numC{n: 4}}}}, []Binding{})
-	if !reflect.DeepEqual(result, closV{args: []string{"x"}, body: appC{fun: idC{s: "+"}, args: []ExprC{idC{s: "x"}, numC{n: 4}}}, env: []Binding{}}) {
+	result := interp(LamC{Args: []string{"x"}, Body: AppC{Fun: IdC{S: "+"}, Args: []ExprC{IdC{S: "x"}, NumC{N: 4}}}}, []Binding{})
+	if !reflect.DeepEqual(result, ClosV{Args: []string{"x"}, Body: AppC{Fun: IdC{S: "+"}, Args: []ExprC{IdC{S: "x"}, NumC{N: 4}}}, Env: []Binding{}}) {
 
 		t.Error("LamC error")
 	}
 }
 
 func TestLookUpBool(t *testing.T) {
-	result := interp(idC{s: "true"}, []Binding{{name: "true", val: true}})
-	if result != true {
+	result := interp(IdC{S: "true"}, []Binding{{Name: "true", Val: BoolV{Bool: true}}})
+	if (result != BoolV{Bool: true}) {
 		t.Error("BoolV error")
 	}
 
 }
 
-func TestArith1(t *testing.T) {
-	testExprC1 := appC{fun: idC{s: "+"}, args: []ExprC{numC{n: 3}, numC{n: 4}}}
-	result := interp(testExprC1, topEnv)
-	if result.(float64) != 7 {
-		t.Error("plus error")
+func TestAdd(t *testing.T) {
+	result := interp(appC{fun: idC{s: "+"}, args: []ExprC{numC{n: 3}, numC{n: 4}}}, topEnv)
+	if result != 7 {
+		t.Error("Add error")
 	}
 }
 
-func TestArith2(t *testing.T) {
-	testExprC1 := appC{fun: idC{s: "-"}, args: []ExprC{numC{n: 3}, numC{n: 4}}}
-	result := interp(testExprC1, topEnv)
-	if result.(float64) != -1 {
-		t.Error("plus error")
+func TestSub(t *testing.T) {
+	result := interp(appC{fun: idC{s: "+"}, args: []ExprC{numC{n: 3}, numC{n: 4}}}, topEnv)
+	if result != 7 {
+		t.Error("Add error")
 	}
 }
-func TestArith3(t *testing.T) {
-	testExprC1 := appC{fun: idC{s: "/"}, args: []ExprC{numC{n: 3}, numC{n: 4}}}
-	result := interp(testExprC1, topEnv)
-	if result.(float64) != 0.75 {
-		t.Error("plus error")
+func TestDiv(t *testing.T) {
+	result := interp(appC{fun: idC{s: "/"}, args: []ExprC{numC{n: 3}, numC{n: 4}}}, topEnv)
+	if result != 0.75 {
+		t.Error("Sub error")
 	}
 }
-func TestArith4(t *testing.T) {
-	testExprC1 := appC{fun: idC{s: "*"}, args: []ExprC{numC{n: 3}, numC{n: 4}}}
-	result := interp(testExprC1, topEnv)
-	if result.(float64) != 12 {
-		t.Error("plus error")
+func TestMult(t *testing.T) {
+	result := interp(appC{fun: idC{s: "*"}, args: []ExprC{numC{n: 3}, numC{n: 4}}}, topEnv)
+	if result != 12 {
+		t.Error("Mult error")
 	}
 }
-func TestArith5(t *testing.T) {
-	testExprC1 := appC{fun: idC{s: "<="}, args: []ExprC{numC{n: 3}, numC{n: 4}}}
-	result := interp(testExprC1, topEnv)
-	if result != true {
-		t.Error("plus error")
+func TestAdd(t *testing.T) {
+	result := interp(appC{fun: idC{s: "+"}, args: []ExprC{numC{n: 3}, numC{n: 4}}}, topEnv)
+	if result != 7 {
+		t.Error("Add error")
 	}
 }
